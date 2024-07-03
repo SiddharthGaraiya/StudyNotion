43c5c2bb2af8a44c0b71c48a8dc9e780763d28ed
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-03 21:24:28 +0530

    Update index.js | created default route and listen server

--- server/index.js
+++ server/index.js
@@ -44,4 +44,16 @@ app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
 app.use("/api/v1/reach", contactUsRoute);
 
+//def route
+
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`App is running at ${PORT}`)
+})
 
