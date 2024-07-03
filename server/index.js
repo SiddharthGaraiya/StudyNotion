d1b50c890f4b9b1523b9f063ce321f5da1d7d77f
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-03 21:23:18 +0530

    setup server configuration and middlewares

---
+++ server/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+const contactUsRoute = require("./routes/Contact");
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const {cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+//database connect
+database.connect();
+//middlewares
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
+)
+
+app.use(
+	fileUpload({
+		useTempFiles:true,
+		tempFileDir:"/tmp",
+	})
+)
