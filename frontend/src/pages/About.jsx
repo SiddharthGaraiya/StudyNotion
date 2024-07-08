6d579028e46381af2b12ac018d00f8ae2c5adf3a
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-08 20:37:19 +0530

    Update About.jsx | added Components LearningGrid and ContactFormSectionand

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -104,7 +104,13 @@ const About = () => {
       </section>
 
       <StatsComponenet />
+      <section className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white">
+        <LearningGrid />
+        <ContactFormSection />
+      </section>
+
       
+   
     </div>
   )
 }
