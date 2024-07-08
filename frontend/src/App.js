5db28221a13a6715bd29abd7f827744f9dde3e1d
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-08 20:27:36 +0530

    Update App.js | added route for About Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './pages/Home';
 import {Route,Routes} from 'react-router-dom'
-
+import About from './pages/About';
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
       </Routes>
     </div>
   );
