4d0f4f569b607c16a8f6e636092529b13e47c93d
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-09 20:41:22 +0530

    Update App.js | added routes for Login and Signup Page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,16 @@ import './App.css';
 import Home from './pages/Home';
 import {Route,Routes} from 'react-router-dom'
 import About from './pages/About';
+import Login from './pages/Login'
+import Signup from './pages/Signup'
 function App() {
   return (
     <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       </Routes>
     </div>
   );
