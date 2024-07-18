4cb6c553802b537d104bae990e00c2c2c7b467cf
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-07-18 21:16:43 +0530

    updated Login form added functionality to call AuthAPI of backend

--- frontend/src/components/core/Auth/LoginForm.jsx
+++ frontend/src/components/core/Auth/LoginForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react"
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai"
+import { useDispatch } from "react-redux"
+import { Link, useNavigate } from "react-router-dom"
 
-import { Link } from "react-router-dom"
+import { login } from "../../../services/operations/authAPI"
 
 function LoginForm() {
-  
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
   const [formData, setFormData] = useState({
     email: "",
     password: "",
@@ -22,7 +25,8 @@ function LoginForm() {
   }
 
   const handleOnSubmit = (e) => {
-    // todo
+    e.preventDefault()
+    dispatch(login(email, password, navigate))
   }
 
   return (
