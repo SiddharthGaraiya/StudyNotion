da0bfa71fcf8779483c27c2e088d9f7670205996
SiddharthGaraiya <siddharth.garaiya01@gmail.com>
2024-08-16 09:46:51 +0530

    added copyright

--- frontend/src/components/common/Footer.jsx
+++ frontend/src/components/common/Footer.jsx
@@ -172,7 +172,7 @@ const Footer = () => {
             })}
           </div>
 
-          <div className="text-center">Made with ❤️ CodeHelp © 2023 Studynotion</div>
+          <div className="text-center">Made with ❤️ by Siddharth © 2024 Studynotion</div>
         </div>
       </div>
     </div>
